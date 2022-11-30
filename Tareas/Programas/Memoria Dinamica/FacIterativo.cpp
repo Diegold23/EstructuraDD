e20e4fd29df37483b1f05f57f8e59de6b6d796f6
Diegold23 <111750136+Diegold23@users.noreply.github.com>
2022-11-29 20:25:59 -0600

    Create FacIterativo.cpp

---
+++ Tareas/Programas/Memoria Dinamica/FacIterativo.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+main(){
+	int numero=0;
+	int i,fact=1;
+	printf("Ingrese un numero: ");
+	scanf("%d",&numero);
+	for(i= 1; i<=numero; i++){
+		fact=fact*i;
+	}
+	printf("El factorial del numero es: %d", fact);
+	return 0;
+}
