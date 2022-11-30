712f580144e37ac082f1559fd167f59d27bdf892
Diegold23 <111750136+Diegold23@users.noreply.github.com>
2022-11-29 20:26:23 -0600

    Create FacRecursivo.cpp

---
+++ Tareas/Programas/Memoria Dinamica/FacRecursivo.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int calculafactorial(int numero) {
+    if (numero <= 1){
+        return 1;
+    }
+    else{
+		return numero * calculafactorial(numero - 1); 
+	}   
+}
+int main() {
+    int numero;
+    int factorial;
+    printf("Ingrese un numero: ");
+    scanf("%d",&numero);
+    factorial=calculafactorial(numero);
+    printf("El factorial de %d es %d", numero, factorial);
+    return 0;
+}
